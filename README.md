10bf1156bfcaf84ac9b8fa215c8fcf0574c2c4d0
snajder <rene.snajder@gmail.com>
2021-05-14 15:37:52 +0200

    Switched to snajder-r/versipy which allows for lists

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# NanoEpiSeg 0.0.3.dev1
+# NanoEpiSeg 0.0.3.dev3
 
 NanoEpiSeg is a tool for *de novo* segmentation of  a methylome from read-level methylation calls (such as Nanopolish).
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 setup(
     name="nanoepiseg",
     description="Methylome segmentation algorithm using a changepoint detection HMM",
-    version="0.0.3.dev1",
+    version="0.0.3.dev3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/snajder-r/nanoepiseg",
@@ -19,8 +19,19 @@ setup(
     author_email="r.snajder@dkfz-heidelberg.de",
     license="MIT",
     python_requires=">=3.7",
-    classifiers=["Development Status :: 3 - Alpha", "Intended Audience :: Science/Research", "Topic :: Scientific/Engineering :: Bio-Informatics", "License :: OSI Approved :: MIT License", "Programming Language :: Python :: 3"],
-    install_requires=["numpy>=1.19.2", "scipy==1.4.1", "pandas>=1.1.3", "meth5>=0.2.7"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    install_requires=[
+        "numpy>=1.19.2",
+        "scipy==1.4.1",
+        "pandas>=1.1.3",
+        "meth5>=0.2.7"
+    ],
     packages=["nanoepiseg"],
     package_dir={"nanoepiseg": "nanoepiseg"},
     entry_points={"console_scripts": ["nanoepiseg=nanoepiseg.__main__:main"]},
